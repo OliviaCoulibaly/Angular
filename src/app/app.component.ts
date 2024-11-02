8ed7a380dffa471d490e9134b9415999861d134e
dcoulibal <fernandecoulibaly5@gmail.com>
2024-11-02 14:29:53 +0000

    reactivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { map } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+<h1>Angular RxJs (Observable)</h1>
+<ul>
+  <li *ngFor="let post of posts$ | async">
+    {{post}} - {{post.body}}
+  </li>
+</ul>
 
-    <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'reactivity';
+  private http = inject(HttpClient);
+  posts$ = this.http.get <any[]>('https://jsonplaceholder.typicode.com/posts');
+
+  ngOnInit(){
+   this.posts$ = this.posts$.pipe(
+    map((posts) => ['Olivia', 'Coulibaly', ...posts]),
+  );
+  }
 }
